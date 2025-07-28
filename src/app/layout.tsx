bd6fc92ed76dd97864ebec7fb2ff8e74570085fd
jayasurya <jayasurya@sparkouttech.com>
2025-07-28 11:21:30 +0530

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Arbitrage Bot",
+  description: "Arbitrage trading is a profitable strategy where you can take advantage of price differences across different exchanges. By using Arbitrage Bots, traders can buy cryptocurrency at a lower price from one exchange and sell it at a higher price on another.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}

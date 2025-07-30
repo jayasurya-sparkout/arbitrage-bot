7a5165738fa3d53548e4dce2f5728e7a84dde9d1
jayasurya <jayasurya@sparkouttech.com>
2025-07-30 12:55:04 +0530

    feat : dashboard page - removed unwantd contents

--- app/user/dashboard/page.tsx
+++ app/user/dashboard/page.tsx
@@ -1,17 +1,11 @@
 'use client';
 
-import { ConnectExchange } from "../../../components/connect-exchange";
-import { ConnectedExchanges } from "../../../components/connectd-exchanges";
 import { ExchangeCounts } from "../../../components/exchange-counts";
-import { BotConfiguration } from "../../../components/bot-configuration";
 
 export default function Page() {
   return (
     <>
-      <ConnectExchange />
-      <ConnectedExchanges />
       <ExchangeCounts />
-      <BotConfiguration />
     </>
   )
 }
